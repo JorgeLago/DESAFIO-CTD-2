8e4ddece7ad8c2b0482f9a4d56177ce96b4a44a1
Jorge Lago <jorgelago92@gmail.com>
2022-11-05 00:47:40 -0300

    Atualizando Novos Desafios

--- DESAFIO2.py
+++ DESAFIO2.py
@@ -1,3 +1,10 @@
-#Escreva um programa que pergunte o salário de um funcionário e 
+# Escreva um programa que pergunte o salário de um funcionário e
 # calcule o valor do seu aumento. Para salários superiores a R$1.250,00
 # , calcule um aumento de 10%. Para inferiores ou iguais, o aumento é 15%.
+salario = float(input('Qual o salário do funcionário? > '))
+
+if salario > 1250:
+    print('O salário teve aumento e reajustou para R${}.'.format(salario + (salario * 0.10)))
+
+else:
+    print('O salário teve aumento e reajustou para R${}.'.format(salario + (salario * 0.15)))
