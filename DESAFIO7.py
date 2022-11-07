065a06f0c3af22b0f4dc1fc30c8c1087559fdbe9
Jorge Lago <jorgelago92@gmail.com>
2022-11-06 22:38:52 -0300

    Adicionando Desafio7.

---
+++ DESAFIO7.py
@@ -0,0 +1,4 @@
+# Melhore o jogo do DESAFIO028 onde o computador  vai 'pensar'
+# em um numero entre 0 a 10. Só que agora o jogador vai tentar
+# adivinhar até acertar, mostrando no final quantos palpites
+# foram necessários para vencer.
