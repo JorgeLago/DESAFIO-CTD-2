927156da0f90a2ee9e6990620cbeb5e7a7f5c741
Jorge Lago <jorgelago92@gmail.com>
2022-11-07 22:26:03 -0300

    Adicionando Desafio

--- DESAFIO8.py
+++ DESAFIO8.py
@@ -5,3 +5,8 @@
 # [4]novos números
 # [5]sair do programa
 # Seu prgrama deverá realizar o operação solicitada em cada caso.
+num = ()
+for i in range(0,2):
+    num = input('Digite um numero: ')
+        num.append(i)
+print(num)
