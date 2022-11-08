c3bafbdd062c318b52bb670133deef050e8dc795
Jorge Lago <jorgelago92@gmail.com>
2022-11-07 22:37:49 -0300

    Atualizando Desafio

--- DESAFIO8.py
+++ DESAFIO8.py
@@ -5,8 +5,21 @@
 # [4]novos números
 # [5]sair do programa
 # Seu prgrama deverá realizar o operação solicitada em cada caso.
-num = ()
-for i in range(0,2):
-    num = input('Digite um numero: ')
-        num.append(i)
-print(num)
+num1 = input(' Digite o numero 1: ')
+num2 = input(' Digite o numero 2: ')
+escolha = 0
+while escolha == 0:
+    if escolha == 1:
+        print(' A soma resulta em {}.'.format(num1+num2))
+    elif escolha == 2:
+        print(' A multiplicação resulta em {}.'.format(num1 * num2))
+    elif escolha == 3:
+        print(' O maior número é {}.'.format(max(num1, num2)))
+    elif escolha == 4:
+        num1 = input(' digite um novo numero:' )
+        num2 = input(' Digite um novo numero2: ')
+    elif escolha == 5:
+        break
+    escolha = input(' Oq deseja fazer: 1 somar, 2 multiplicar, 3 qual o maior, 4 novos numeros, 5 Fim!')
+
+print('Fim do Programa!!')
