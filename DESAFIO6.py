e0c6b7714bd324aef37b765507b05774f8f21354
Jorge Lago <jorgelago92@gmail.com>
2022-11-08 22:50:01 -0300

    Desafio 6 Concluído

--- DESAFIO6.py
+++ DESAFIO6.py
@@ -1,3 +1,14 @@
 #Faça um programa que leia o sexo de uma pessoa, mas só aceite
 # os valores 'M' ou 'F'. Caso esteja errado, peça a digitação
 # novamente até ter um valor correto.
+pessoa = 'incorreto'
+while pessoa == 'incorreto':
+    pessoa = str(input(('Digite qual seu sexo ( com M ou F ): '))).upper()
+    if pessoa == 'M':
+        print(' Você escolheu Sexo masculino (M)!')
+    elif pessoa == 'F':
+        print(('Você escolheu Sexo Feminino (F) !'))
+    else:
+        print('Alternativa incorreta, responda com M ou F.')
+        pessoa = 'incorreto'
+print( ' Fim do Programa!!')
