0fa13c069d9e4c61baee5ac57178275cc154c5b7
Jorge Lago <jorgelago92@gmail.com>
2022-11-09 21:39:51 -0300

    Atualizando desafio 4

--- DESAFIO4.py
+++ DESAFIO4.py
@@ -1,9 +1,3 @@
 # Faça um programa que calcule a soma entre todos os números ímpares que
 # são multiplos de três e que se
 # encontram no intervalo de 1 até 500.
-multi = 0
-for multi_tres in range(0, 1000, 3):
-    if multi_tres % 2 == 1:
-        multi += multi_tres
-
-print(multi)
