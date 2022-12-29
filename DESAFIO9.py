2de0d71d76c333f94674a6577d0961abbb5fd5d2
Jorge Lago <jorgelago92@gmail.com>
2022-12-29 16:04:28 -0300

    atualizando

--- DESAFIO9.py
+++ DESAFIO9.py
@@ -1,2 +1,7 @@
 # Faça um programa que leia um número qualquer e mostre o seu factorial.
 #Ex.: 5!=5x4x3x2x1=120
+numero = int(input(' Digite um número:  '))
+fatorial = numero
+for i in range(0,numero):
+    fatorial *= i
+print(fatorial)
